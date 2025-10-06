7f94ba969da5c05e227a2be6fb3448c107ac550c
Khanh Tang <quockhanhtanghuynh@gmail.com>
2025-10-06 22:36:41 +0700

    DTO

--- HospitalManagerment/DTO/AccountDTO.cs
+++ HospitalManagerment/DTO/AccountDTO.cs
@@ -8,5 +8,58 @@ namespace HospitalManagerment.DTO
 {
     internal class AccountDTO
     {
+        private string tenDangNhap;
+        private string matKhau;
+        private string maQuyen;
+        private string maNV;
+        private string trangThaiXoa;
+
+        public AccountDTO()
+        {
+            this.tenDangNhap = " ";
+            this.matKhau = " ";
+            this.maQuyen = " ";
+            this.maNV = " ";
+            this.trangThaiXoa = " ";
+        }
+
+        public AccountDTO(string tenDangNhap, string matKhau, string maQuyen, string maNV, string trangThaiXoa)
+        {
+            this.tenDangNhap = tenDangNhap;
+            this.matKhau = matKhau;
+            this.maQuyen = maQuyen;
+            this.maNV = maNV;
+            this.trangThaiXoa = trangThaiXoa;
+        }
+
+        public string TenDangNhap
+        {
+            get => this.tenDangNhap;
+            set { this.tenDangNhap = value; }
+        }
+
+        public string MatKhau
+        {
+            get => this.matKhau;
+            set { this.matKhau = value; }
+        }
+
+        public string MaQuyen
+        {
+            get => this.maQuyen;
+            set { this.maQuyen = value; }
+        }
+
+        public string MaNV
+        {
+            get => this.maNV;
+            set { this.maNV = value; }
+        }
+
+        public string TrangThaiXoa
+        {
+            get => this.trangThaiXoa;
+            set { this.trangThaiXoa = value; }
+        }
     }
 }
