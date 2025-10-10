32ae793e3cd3db5255e2f56a47a525582a4734fa
Khanh Tang <quockhanhtanghuynh@gmail.com>
2025-10-10 16:02:13 +0700

    GetAllAcounts

--- HospitalManagerment/DAO/AccountDAO.cs
+++ HospitalManagerment/DAO/AccountDAO.cs
@@ -37,5 +37,31 @@ namespace HospitalManagerment.DAO
             errorMessage = "Tên đăng nhập hoặc mật khẩu không đúng!";
             return result;
         }
+
+        public bool getAllAccount(out String errorMessage)
+        {
+            bool result = false;
+            using (MySqlConnection conn = DatabaseConnection.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT * FROM tai_khoan WHERE TenDangNhap=@username AND MatKhau=@password";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@username", account.TenDangNhap);
+                    cmd.Parameters.AddWithValue("@password", account.MatKhau);
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            errorMessage = "Đăng nhập thành công!";
+                            return true;
+
+                        }
+                    }
+                }
+            }
+            errorMessage = "Tên đăng nhập hoặc mật khẩu không đúng!";
+            return result;
+        }
     }
 }
